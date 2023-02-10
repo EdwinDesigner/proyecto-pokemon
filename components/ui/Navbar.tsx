2f19f2a1fc6b81d302d289ace92feaa3632f5b3d
Edwin Hernández <edwinhernandez.br@gmail.com>
2023-02-10 08:03:18 -0400

    guardando en stage

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Spacer, Text, useTheme } from "@nextui-org/react"
+import Link from "next/link";
 
 export const Navbar = () => {
 
@@ -15,19 +16,28 @@ export const Navbar = () => {
             padding: "0px 20px",
             backgroundColor: theme?.colors.gray100.value
         }}>
-            <Image
-                width={ 70 }
-                height={ 70 }
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-                alt="Icon App"
-            />
+            <Link
+                href='/'
+                style={{
+                    display: "flex",
+                }}
+            >
+                <Image
+                    width={ 70 }
+                    height={ 70 }
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+                    alt="Icon App"
+                />
 
-            <Text color="white" h2>P</Text>
-            <Text color="white" h3>okemón</Text>
+                <Text color="white" h2>P</Text>
+                <Text color="white" h3>okemón</Text>
+            </Link>
 
             <Spacer css={{ flex: 1}} />
 
-            <Text color="white">Favoritos</Text>
+            <Link href="/favorites">
+                <Text color="white">Favoritos</Text>
+            </Link>
         </div>
     )
 }
